e5de5f63967144a3629a0778f11505ef0a42ba19
ZP <syszhaopan@gmail.com>
2025-06-04 17:35:31 +0800

    Update install-serverd.sh

--- install-serverd.sh
+++ install-serverd.sh
@@ -2,5 +2,13 @@
 
 systemctl daemon-reload
 systemctl start webhook-handler
+systemctl restart webhook-handler
 systemctl status webhook-handler
 #systemctl enable webhook-handler
+
+# log
+#journalctl -u webhook-handler -f
+
+# stop & uninstall
+# systemctl stop webhook-handler
+# systemctl disable webhook-handler
